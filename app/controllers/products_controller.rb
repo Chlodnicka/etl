76ed9bb1710ee614df72d927c092fe07c3f6359a
Chlodnicka <chlodnicka.maja@gmail.com>
2016-11-22 19:39:34 +0100

    Nokogiri, getting data from ceneo, parsing reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,15 +26,18 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
+    product_info = get_product(@product)
+    reviews = get_reviews(@product)
+
+    # respond_to do |format|
+    #    if @product.save
+    #     format.html { redirect_to @product, notice: 'Product was successfully created.' }
+    #    format.json { render :show, status: :created, location: @product }
+    #  else
+    #    format.html { render :new }
+    #    format.json { render json: @product.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /products/1
@@ -61,14 +64,97 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  def get_product(product)
+    require 'open-uri'
+    filename = 'http://www.ceneo.pl/' + product.code + '#tab=spec'
+    doc = Nokogiri::HTML(open(filename))
+    puts doc.css('.breadcrumb:last-of-type a span').text
+    puts doc.css('.ProductSublineTags').text
+    puts doc.css('.specs-group:first-of-type table tbody tr:first-of-type td ul li a').text
+    puts doc.css('h1.product-name').text
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:code, :brand, :model, :type, :notes)
+  def get_reviews(product)
+    require 'open-uri'
+    filename = 'http://www.ceneo.pl/' + product.code + '#tab=reviews'
+    doc = Nokogiri::HTML(open(filename))
+
+    reviews = doc.css('.product-review')
+    reviews_content = []
+    i = 0
+    reviews.each { |review|
+      reviews_content[i] = get_review(review)
+      i +=1
+    }
+
+    pagination = doc.css('.pagination ul li a')
+    pag_count = pagination.size
+    if pag_count > 1
+      pagination.each { |site|
+        filename = 'http://www.ceneo.pl/' + site['href']
+        doc = Nokogiri::HTML(open(filename))
+        reviews = doc.css('.product-review')
+        reviews.each { |review|
+          reviews_content[i] = get_review(review)
+          i +=1
+        }
+      }
     end
+    puts reviews_content
+  end
+
+  def get_review(review)
+
+    require 'json'
+
+    pros_array = cons_array = []
+    i = j = 0
+
+    pros = review.css('.pros-cell ul li')
+    pros.each { |value|
+      pros_array[i] = value.text
+      i +=1
+    }
+
+    cons = review.css('.cons-cell ul li')
+    cons.each { |value|
+      cons_array[j] = value.text
+      j +=1
+    }
+
+    date = review.css('.review-time time')
+
+    score = review.css('.review-score-count').text
+
+    code = review.css('.vote-yes')
+
+    [
+        'author' => review.css('.product-reviewer').text,
+        'pros' => pros_array.to_json,
+        'cons' => cons_array.to_json,
+        'summary' => review.css('.product-review-body').text,
+        'score' => score[0...1],
+        'time' => date[0]['datetime'],
+        'recommendation' => review.css('.product-recommended').text,
+        'useful' => review.css('.vote-yes span').text,
+        'not_useful' => review.css('.vote-no span').text,
+        'code' => code[0]['data-review-id']
+    ]
+
+  end
+
+  def parse_product()
+
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def product_params
+    params.require(:product).permit(:code, :brand, :model, :type, :notes)
+  end
 end
