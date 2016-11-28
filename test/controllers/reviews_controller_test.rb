f9f5ac8b998a0021b584cbf80bb3c7a4d6cfa44f
Chlodnicka <chlodnicka.maja@gmail.com>
2016-11-28 16:49:42 +0100

    Update in tests, removal of unneccessary code

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -11,24 +11,4 @@ class ReviewsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:reviews)
   end
 
-  test "should create review" do
-    assert_difference('Review.count') do
-      post :create, review: { author: @review.author, cons: @review.cons, not_useful: @review.not_useful, product_id: @review.product_id, pros: @review.pros, recommendation: @review.recommendation, time: @review.time, summary: @review.summary, useful: @review.useful }
-    end
-
-    assert_redirected_to review_path(assigns(:review))
-  end
-
-  test "should update review" do
-    patch :update, id: @review, review: { author: @review.author, cons: @review.cons, not_useful: @review.not_useful, product_id: @review.product_id, pros: @review.pros, recommendation: @review.recommendation, time: @review.time, summary: @review.summary, useful: @review.useful }
-    assert_redirected_to review_path(assigns(:review))
-  end
-
-  test "should destroy review" do
-    assert_difference('Review.count', -1) do
-      delete :destroy, id: @review
-    end
-
-    assert_redirected_to reviews_path
-  end
 end
